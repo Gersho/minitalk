c0bf6a995c0e5b773f824fe85159f2237c2b0e10
Zennoune Karim <kzennoun@student.42lyon.fr>
2021-06-07 13:34:35 +0200

    done

--- srcs/get_message.c
+++ srcs/get_message.c
@@ -6,49 +6,60 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 14:01:26 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/06/07 12:42:29 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/06/07 13:09:18 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	init_str(char **str)
+{
+	if (!*str)
+	{
+		*str = malloc(sizeof(char *));
+		if (!*str)
+			error_exit(-1);
+		*str[0] = '\0';
+	}
+}
+
+void	char_complete(char **str, char *c)
+{
+	char	*tmp;
+
+	if (*c == '\0')
+	{
+		ft_putstr_fd(*str, 1);
+		free(*str);
+		*str = NULL;
+	}
+	else
+	{
+		tmp = ft_straddchar(*str, *c);
+		if (!tmp)
+		{
+			free(*str);
+			error_exit(-1);
+		}
+		free(*str);
+		*str = tmp;
+	}
+	*c = '\0';
+}
+
 void	get_message(int sig)
 {
 	static char	c;
 	static char	*str;
-	char		*tmp;
 	static int	i;
 
-	if (!str)
-	{
-		str = malloc(sizeof(char *));
-		if (!str)
-			error_exit(-1);
-		str[0] = '\0';
-	}
+	init_str(&str);
 	if (sig == SIGUSR1)
 		c |= 128 / ft_power(2, i);
 	i++;
 	if (i == 8)
 	{
 		i = 0;
-		if (c == '\0')
-		{
-			ft_putstr_fd(str, 1);
-			free(str);
-			str = NULL;
-		}
-		else
-		{
-			tmp = ft_straddchar(str, c);
-			if (!tmp)
-			{
-				free(str);
-				error_exit(-1);
-			}
-			free(str);
-			str = tmp;
-		}
-		c = '\0';
+		char_complete(&str, &c);
 	}
 }
--- srcs/send_message.c
+++ srcs/send_message.c
@@ -6,67 +6,43 @@
 /*   By: kzennoun <kzennoun@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 12:26:00 by kzennoun          #+#    #+#             */
-/*   Updated: 2021/06/07 12:50:11 by kzennoun         ###   ########lyon.fr   */
+/*   Updated: 2021/06/07 12:55:04 by kzennoun         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
+static void	send_char(char c, int pid)
+{
+	int	j;
 
-// static void	send_char(char c, int pid)
-// {
-// 	int j;
-
-// 	j = 0;
-// 	while (j < 8)
-// 	{
-// 		if (str[i] & 128 / ft_power(2, j))
-// 		{
-// 			if (kill(pid, SIGUSR1) == -1)
-// 				error_exit(-1);
-// 		}
-// 		else
-// 		{
-// 			if (kill(pid, SIGUSR2) == -1)
-// 				error_exit(-1);
-// 		}
-// 		usleep(100);
-// 		j++;
-// 	}
-// }
+	j = 0;
+	while (j < 8)
+	{
+		if (c & 128 / ft_power(2, j))
+		{
+			if (kill(pid, SIGUSR1) == -1)
+				error_exit(-1);
+		}
+		else
+		{
+			if (kill(pid, SIGUSR2) == -1)
+				error_exit(-1);
+		}
+		usleep(100);
+		j++;
+	}
+}
 
 void	send_message(char *str, int pid)
 {
 	int	i;
-	int	j;
 
 	i = 0;
 	while (str[i])
 	{
-		j = 0;
-		while (j < 8)
-		{
-			if (str[i] & 128 / ft_power(2, j))
-			{
-				if (kill(pid, SIGUSR1) == -1)
-					error_exit(-1);
-			}
-			else
-			{
-				if (kill(pid, SIGUSR2) == -1)
-					error_exit(-1);
-			}
-			usleep(100);
-			j++;
-		}
+		send_char(str[i], pid);
 		i++;
 	}
-	j = 0;
-	while (j < 8)
-	{
-		if (kill(pid, SIGUSR2) == -1)
-			error_exit(-1);
-		usleep(100);
-		j++;
-	}
+	send_char('\0', pid);
 }
